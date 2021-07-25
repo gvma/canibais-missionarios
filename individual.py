d8c9655db2762254b117bc6ac7d57f31e6c16347
João Victor Ribeiro Ferro <39158742+joaovictorferro@users.noreply.github.com>
2021-07-25 14:59:31 -0300

    Add files via upload

--- individual.py
+++ individual.py
@@ -1,27 +1,25 @@
-from chromosome import Chromosome
 from random import randint
+from copy import copy
+from chromosome import Chromosome
 
 persons = ['canibal', 'missionario']
 
 class Individual:
 
-  def __init__(self, chromosomes = None):
+  def __init__(self):
     self.score = 0
-    if chromosomes is None:
-      self.chromosomes = []
-      for i in range(11):
-        # sorteando para ver quantas pessoas vao passar de uma margem para outra
-        config = {
-          'people': []
-        }
-        config['number_of_people'] = randint(1, 2)
+    self.chromosomes = []
+    for i in range(11):
+      #   # sorteando para ver quantas pessoas vao passar de uma margem para outra
+      config = {
+        'people': []
+      }
+      config['number_of_people'] = randint(1, 2)
+      config['people'].append(persons[randint(0, 1)])
+      if config['number_of_people'] == 2:
         config['people'].append(persons[randint(0, 1)])
-        if config['number_of_people'] == 2:
-          config['people'].append(persons[randint(0, 1)])
-        chromosome = Chromosome(config)
-        self.chromosomes.append(chromosome)
-    else:
-      self.chromosomes = chromosomes
+      chromosome = Chromosome(config)
+      self.chromosomes.append(chromosome)
 
   def __str__(self) :
     chromosomes = ''
--- main.py
+++ main.py
@@ -1,53 +1,24 @@
-# 3 canibais e 3 missionarios na mesma margem
 import random
 from random import seed
 from random import *
 from individual import Individual
 
-population = []
-newpopulation = []
-score = 0
 
-def selection(populacao,novapopulacao):
-    qualquerLista = populacao + novapopulacao
-    qualquerLista = sorted(qualquerLista, key=lambda ind: ind.score, reverse=True)
-    selected_population = []
-    for i in range(50):
-        selected_population.append(qualquerLista[i])
-    return selected_population
-
-def mutacao(novaPopulacao):
-    population_mutation = randint(1,5)
-
-    for i in range(population_mutation):
-        ind_index = randint(0,len(novaPopulacao)-1)
-        individual = novaPopulacao[ind_index]
-        pos_chromosomes = randint(0,10)
-        print(individual)
-        if individual.chromosomes[pos_chromosomes].config['people'] =="canibal":
-            individual.chromosomes[pos_chromosomes].config['people'] = "missionario"
-        else:
-            individual.chromosomes[pos_chromosomes].config['people'] = "canibal"
-
-
-def cruzamentoParte2(pai,mae,novaPopulacao):
+def cruzamentoParte2(pai,mae,newPopulation):
     part1 = randint(1,10)
     individuo1 = Individual(pai.chromosomes[:part1] + mae.chromosomes[part1:])
     individuo2 = Individual(mae.chromosomes[:part1] + pai.chromosomes[part1:])
-    novaPopulacao.append(individuo1)
-    novaPopulacao.append(individuo2)
+    newPopulation.append(individuo1)
+    newPopulation.append(individuo2)
 
-def cruzamentoParte1(populacao,novaPopulacao):
-    population_cross_over = randint(10,len(populacao))
-    for i in range(population_cross_over):
-        x = randint(0,len(populacao)-1)
-        y = randint(0,len(populacao)-1)
-        while x == y:
-            y = randint(0, len(populacao) - 1)
-        pai = populacao[x]
-        mae = populacao[y]
-        cruzamentoParte2(pai,mae,novaPopulacao)
-    return novaPopulacao
+def cruzamentoParte1(populacao,newPopulation):
+    for i in range(len(populacao)-1):
+        for j in range(len(populacao)-1):
+            porcentagem = randint(0,100)
+            if  porcentagem >= 0 and i != j:
+                pai = populacao[i]
+                mae = populacao[j]
+                cruzamentoParte2(pai,mae,newPopulation)
 
 def get_right_margin_people(position,left_margin_canibals, left_margin_missionaries, right_margin_canibals, right_margin_missionaries, config):
   if config['people'][position] == "missionario":
@@ -82,12 +53,6 @@ def validate(position,  left_margin_canibals, left_margin_missionaries, right_ma
       score -= 1
   return score
 
-def init_population():
-  for i in range(50):
-    individual = Individual()
-    population.append(individual)
-    print(individual)
-
 def score(population):
   for i in range(len(population)):
     score = 11
@@ -100,7 +65,7 @@ def score(population):
 
     for j in range(len(chromosomes)):
       config = chromosomes[j].config
-      print(config)
+      #print(config)
 
       if j%2 == 0:
         if config['people'][0] == 'missionario':
@@ -187,22 +152,28 @@ def score(population):
       else:
         score = validate(0, left_margin_canibals, left_margin_missionaries, right_margin_canibals,
                          right_margin_missionaries, config, score)
-      print("SCORE " + str(score))
+
+    if score == 11 and left_margin_missionaries != 0 and left_margin_canibals != 0:
+        score = -11
+    print("SCORE " + str(score))
     population[i].score = score
 
-init_population()
+def init_population(population):
+  for i in range(2):
+    individual = Individual()
+    population.append(individual)
+    #print(individual)
 
-while True:
-    flag = False
-    score(population)
+def main():
+    population = []
+    newPopulation=[]
+
+    init_population(population)
     for ind in population:
-        if ind.score == 11:
-            print(ind)
-            flag = True
-            break
-    if flag:
-        break
-    newpopulation = cruzamentoParte1(population,[])
-    mutacao(newpopulation)
-    score(newpopulation)
-    population = selection(population,newpopulation)
+        print(ind)
+    score(population)
+    cruzamentoParte1(population,newPopulation)
+    print(newPopulation)
+
+if __name__ == '__main__':
+    main()

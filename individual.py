7625a0c618b5b039d79b74778ba5cfb9e02510b6
João Victor Ribeiro Ferro <39158742+joaovictorferro@users.noreply.github.com>
2021-07-26 08:17:46 -0300

    att

--- individual.py
+++ individual.py
@@ -1,25 +1,27 @@
-from random import randint
-from copy import copy
 from chromosome import Chromosome
+from random import randint
 
 persons = ['canibal', 'missionario']
 
 class Individual:
 
-  def __init__(self):
+  def __init__(self, chromosomes = None):
     self.score = 0
-    self.chromosomes = []
-    for i in range(11):
-      #   # sorteando para ver quantas pessoas vao passar de uma margem para outra
-      config = {
-        'people': []
-      }
-      config['number_of_people'] = randint(1, 2)
-      config['people'].append(persons[randint(0, 1)])
-      if config['number_of_people'] == 2:
+    if chromosomes is None:
+      self.chromosomes = []
+      for i in range(11):
+        # sorteando para ver quantas pessoas vao passar de uma margem para outra
+        config = {
+          'people': []
+        }
+        config['number_of_people'] = randint(1, 2)
         config['people'].append(persons[randint(0, 1)])
-      chromosome = Chromosome(config)
-      self.chromosomes.append(chromosome)
+        if config['number_of_people'] == 2:
+          config['people'].append(persons[randint(0, 1)])
+        chromosome = Chromosome(config)
+        self.chromosomes.append(chromosome)
+    else:
+      self.chromosomes = chromosomes
 
   def __str__(self) :
     chromosomes = ''
--- main.py
+++ main.py
@@ -1,24 +1,52 @@
-import random
+# 3 canibais e 3 missionarios na mesma margem
 from random import seed
 from random import *
 from individual import Individual
 
+population = []
+newpopulation = []
+score = 0
 
-def cruzamentoParte2(pai,mae,newPopulation):
+def selection():
+    qualquerLista = population + newpopulation
+    qualquerLista = sorted(qualquerLista, key=lambda ind: ind.score, reverse=True)
+    selected_population = []
+    for i in range(50):
+        selected_population.append(qualquerLista[i])
+    return selected_population
+
+def mutacao():
+    population_mutation = 5
+
+    for i in range(population_mutation):
+        ind_index = randint(0,len(newpopulation)-1)
+        print("IND INDEX " + str(ind_index) + "TAMANHO " + str(ind_index))
+        individual = newpopulation[ind_index]
+        pos_chromosomes = 5
+        #print(individual.chromosomes[pos_chromosomes].config['people'])
+        if individual.chromosomes[pos_chromosomes].config['people'][0] =="canibal":
+           # print("ENTREI AQUI CANIBAL ~~~~~~~~~~~~~~~~~~~~")
+            individual.chromosomes[pos_chromosomes].config['people'][0] = "missionario"
+        else:
+            individual.chromosomes[pos_chromosomes].config['people'][0] = "canibal"
+            #print("ENTREI AQUI MISSIONARIO~~~~~~~~~~~~~~~~~~~~")
+
+
+def cruzamentoParte2(pai,mae):
     part1 = randint(1,10)
     individuo1 = Individual(pai.chromosomes[:part1] + mae.chromosomes[part1:])
     individuo2 = Individual(mae.chromosomes[:part1] + pai.chromosomes[part1:])
-    newPopulation.append(individuo1)
-    newPopulation.append(individuo2)
+    newpopulation.append(individuo1)
+    newpopulation.append(individuo2)
 
-def cruzamentoParte1(populacao,newPopulation):
-    for i in range(len(populacao)-1):
-        for j in range(len(populacao)-1):
+def cruzamentoParte1():
+    for i in range(len(population)):
+        for j in range(len(population)):
             porcentagem = randint(0,100)
-            if  porcentagem >= 0 and i != j:
-                pai = populacao[i]
-                mae = populacao[j]
-                cruzamentoParte2(pai,mae,newPopulation)
+            if porcentagem >= 60 and i != j:
+                pai = population[i]
+                mae = population[j]
+                cruzamentoParte2(pai,mae)
 
 def get_right_margin_people(position,left_margin_canibals, left_margin_missionaries, right_margin_canibals, right_margin_missionaries, config):
   if config['people'][position] == "missionario":
@@ -40,18 +68,18 @@ def get_left_margin_people(position,left_margin_canibals, left_margin_missionari
     right_margin_canibals += 1
     return left_margin_canibals, right_margin_canibals
 
-def validate(position,  left_margin_canibals, left_margin_missionaries, right_margin_canibals, right_margin_missionaries, config, score):
-  if config['people'][position] == 'missionario':
+def validate(left_margin_canibals, left_margin_missionaries, right_margin_canibals, right_margin_missionaries, score):
     if left_margin_canibals > left_margin_missionaries and left_margin_missionaries != 0:
-      score -= 1
+        score -= 1
     elif right_margin_canibals > right_margin_missionaries and right_margin_missionaries != 0:
-      score -= 1
-  else:
-    if left_margin_canibals  > left_margin_missionaries and left_margin_missionaries != 0:
-      score -= 1
-    elif right_margin_canibals  > right_margin_missionaries and right_margin_missionaries != 0:
-      score -= 1
-  return score
+        score -= 1
+    return score
+
+def init_population():
+  for i in range(50):
+    individual = Individual()
+    population.append(individual)
+    #print(individual)
 
 def score(population):
   for i in range(len(population)):
@@ -59,7 +87,6 @@ def score(population):
     left_margin_canibals = 3
     left_margin_missionaries = 3
     right_margin_canibals = 0
-    #print("Right margin " + str(right_margin_canibals))
     right_margin_missionaries = 0
     chromosomes = population[i].chromosomes
 
@@ -105,14 +132,14 @@ def score(population):
                                                                                left_margin_missionaries,
                                                                                right_margin_canibals,
                                                                                right_margin_missionaries, config)
-
+#===================================== do segundo ====================================================================
       if config['number_of_people'] == 2:
         if j % 2 == 0:
           if config['people'][0] == 'missionario':
               if left_margin_missionaries == 0:
                   score -= 1
               else:
-                left_margin_missionaries, right_margin_missionaries = get_left_margin_people(0, left_margin_canibals,
+                left_margin_missionaries, right_margin_missionaries = get_left_margin_people(1, left_margin_canibals,
                                                                                          left_margin_missionaries,
                                                                                          right_margin_canibals,
                                                                                          right_margin_missionaries,
@@ -121,7 +148,7 @@ def score(population):
               if left_margin_canibals == 0:
                   score -= 1
               else:
-                left_margin_canibals, right_margin_canibals = get_left_margin_people(0, left_margin_canibals,
+                left_margin_canibals, right_margin_canibals = get_left_margin_people(1, left_margin_canibals,
                                                                                  left_margin_missionaries,
                                                                                  right_margin_canibals,
                                                                                  right_margin_missionaries,
@@ -130,7 +157,7 @@ def score(population):
         if j % 2 != 0:
           if config['people'][0] == 'missionario':
               if right_margin_missionaries != 0:
-                  left_margin_missionaries, right_margin_missionaries = get_right_margin_people(0, left_margin_canibals,
+                  left_margin_missionaries, right_margin_missionaries = get_right_margin_people(1, left_margin_canibals,
                                                                                         left_margin_missionaries,
                                                                                         right_margin_canibals,
                                                                                         right_margin_missionaries,
@@ -142,38 +169,45 @@ def score(population):
               if right_margin_canibals == 0:
                   score -= 1
               else:
-                left_margin_canibals, right_margin_canibals = get_right_margin_people(0, left_margin_canibals,
+                left_margin_canibals, right_margin_canibals = get_right_margin_people(1, left_margin_canibals,
                                                                                   left_margin_missionaries,
                                                                                   right_margin_canibals,
                                                                                   right_margin_missionaries, config)
 
-        score = validate(1, left_margin_canibals, left_margin_missionaries, right_margin_canibals,
-                         right_margin_missionaries, config, score)
+        score = validate(left_margin_canibals, left_margin_missionaries, right_margin_canibals,
+                         right_margin_missionaries, score)
       else:
-        score = validate(0, left_margin_canibals, left_margin_missionaries, right_margin_canibals,
-                         right_margin_missionaries, config, score)
+        score = validate(left_margin_canibals, left_margin_missionaries, right_margin_canibals,
+                         right_margin_missionaries, score)
+
 
     if score == 11 and left_margin_missionaries != 0 and left_margin_canibals != 0:
+        print("ENTREI NO -11")
         score = -11
-    print("SCORE " + str(score))
+    #print("SCORE " + str(score))
     population[i].score = score
 
-def init_population(population):
-  for i in range(2):
-    individual = Individual()
-    population.append(individual)
-    #print(individual)
-
-def main():
-    population = []
-    newPopulation=[]
-
-    init_population(population)
-    for ind in population:
-        print(ind)
+init_population()
+b = 0
+while True:
+    flag = False
     score(population)
-    cruzamentoParte1(population,newPopulation)
-    print(newPopulation)
-
-if __name__ == '__main__':
-    main()
+    for ind in population:
+        #print(ind)
+        for ind in population:
+            if ind.score == 11:
+                print(ind)
+                print(ind.score)
+                flag = True
+                break
+    if flag:
+        break
+    cruzamentoParte1()
+    # for ind in newpopulation:
+    #     print(ind)
+    mutacao()
+    score(newpopulation)
+    population = selection()
+    newpopulation.clear()
+    #print("TAMANHO DA POPULACAO" + str(len(population)))
+    b += 1
